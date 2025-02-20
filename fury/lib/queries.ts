9bc1a1934b396a057a7ff1a012938bc5142c7712
jrmph13 <jhames.martin.dev@gmail.com>
2025-02-20 10:56:50 +0000

    fix(api): integrate view templates

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49ddc24469304c4fb9705d9e6c9e5928 = () => {
-  return '2025-02-07T04:44:56Z';
+const node_907091d14ba24a31b0d64ca421cf90d0 = () => {
+  return '2025-02-20T10:56:50Z';
 };
