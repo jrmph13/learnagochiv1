96aabbb036e50c553bca4ebe0e597123c9761fe9
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-01 10:26:06 +0000

    chore(api): update auth tokens

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_441dc9e5f4cc45f4a2b72414c781a8bd = () => {
-  return '2025-02-09T06:24:54Z';
+const node_f986dfe6bb9744f69314990f0095afcb = () => {
+  return '2025-03-01T10:26:06Z';
 };
