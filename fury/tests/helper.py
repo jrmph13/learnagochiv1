cc260b419990f544bbd2d79f1bdfec5b4e894ec0
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-01 18:52:17 +0000

    feat(api): implement payload parser

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f8d31c4787554155bbb414ead73dc7c5 = () => {
-  return '2025-03-01T22:07:13Z';
+const node_aa78c90b8c1b4edaa887a242b46bd175 = () => {
+  return '2025-03-01T18:52:17Z';
 };
