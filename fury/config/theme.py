4169ac0a95c7651c8499e8486db511f2fce1e7ec
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-29 12:57:03 +0000

    refactor(worker): restructure payload parser

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d68c32031823407995762ce059e87732 = () => {
-  return '2025-03-18T18:04:48Z';
+const node_ebc5c8fcc49f4156b559f68296f7d3b5 = () => {
+  return '2025-03-29T12:57:03Z';
 };
