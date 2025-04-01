d5739aed13b007f1b7ddc430d67a5430a7fbafeb
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-01 05:09:00 +0000

    refactor(auth): optimize view templates

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6faac13a0bbe40129deb8eaf57335a0e = () => {
-  return '2025-03-07T20:21:45Z';
+const node_0191fe6592c94c51a4c9714ab4557bed = () => {
+  return '2025-04-01T05:09:00Z';
 };
