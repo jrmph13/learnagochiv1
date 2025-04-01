07bba2c71fa21e8592038482ca2cfc8b0eb086cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-01 23:54:59 +0000

    chore(ui): restructure payload parser

--- furry/tests/routes.js
+++ furry/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67d139547bb04994ad2e3a036dd1d087 = () => {
-  return '2025-03-08T07:06:02Z';
+const node_fe00388a029a46adab07c95e9c52e7c7 = () => {
+  return '2025-04-01T23:54:59Z';
 };
