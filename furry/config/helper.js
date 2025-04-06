6affe359ece46b4e1b8808876054c25fa46b9e58
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-06 13:17:33 +0000

    refactor(config): optimize database index

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91b93161e48a424c84b7c60071231a1d = () => {
-  return '2025-02-28T06:39:48Z';
+const node_0dd46fb361ab47f1ae364144788ed2f0 = () => {
+  return '2025-04-06T13:17:33Z';
 };
