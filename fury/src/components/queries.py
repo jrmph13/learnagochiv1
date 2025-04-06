cd5727b615c3f4b9349da5ccf1174b9c6d257431
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-06 07:30:26 +0000

    refactor(worker): optimize middleware routes

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bde0106f2ad845858d04042f2b97058e = () => {
-  return '2025-04-05T10:50:43Z';
+const node_8a1cd73014974978937f507bfad302d5 = () => {
+  return '2025-04-06T07:30:26Z';
 };
