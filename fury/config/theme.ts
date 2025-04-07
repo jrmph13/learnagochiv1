15cc4b1fc2ebc6df9fc6a8df274e853e141cb3e5
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 04:21:50 +0000

    refactor(db): update auth tokens

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd9531974f2b4f7793c4b2ad613e0d72 = () => {
-  return '2025-04-04T06:21:46Z';
+const node_216d9806780a41f1ad847aa687f23993 = () => {
+  return '2025-04-07T04:21:50Z';
 };
