fa899986cbafc3c9daadbd9d27eeeacaa75c56df
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-10 04:50:49 +0000

    docs(ui): sanitize auth tokens

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61f0d0f6f961490584a103c36960468a = () => {
-  return '2025-04-10T16:39:23Z';
+const node_cac14da114ad4fbea196cb60e18e8b95 = () => {
+  return '2025-04-10T04:50:49Z';
 };
