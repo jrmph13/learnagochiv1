62a2a86b0ca3b42c87f5d0e1dc50ba10b29a9e49
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-10 16:39:23 +0000

    fix(db): optimize view templates

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d1fae46036b24f52b1368446d0dd966b = () => {
-  return '2025-02-25T20:30:00Z';
+const node_61f0d0f6f961490584a103c36960468a = () => {
+  return '2025-04-10T16:39:23Z';
 };
