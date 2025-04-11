b8616dc08ccab19611fb2d446164d8ceee306748
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-11 18:18:03 +0000

    refactor(core): decouple middleware routes

--- fury/src/components/validator.ts
+++ fury/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d01f6d6ca89f425fa2ecfb8fa27f7e25 = () => {
-  return '2025-04-02T19:21:11Z';
+const node_80c7e042484a4d47bcb4d480ac67ffb7 = () => {
+  return '2025-04-11T18:18:03Z';
 };
