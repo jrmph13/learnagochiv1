7773fb3aa7c985ee0f269e09ade71ce92e430a75
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-12 16:31:28 +0000

    fix(auth): integrate view templates

--- furry/src/controllers/index.py
+++ furry/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb1e2f54038a4d9fbf1a74a8f46d983c = () => {
-  return '2025-04-05T07:11:41Z';
+const node_c2e29d117cc647af95fe79d1a52d1d5f = () => {
+  return '2025-04-12T16:31:28Z';
 };
