ce2e164343268a50069fff515443ccd832968e67
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 20:12:13 +0000

    fix(auth): sanitize validation pipeline

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6063fa130aa4c9781e0fde3461e2072 = () => {
-  return '2025-03-22T22:22:04Z';
+const node_ad481236abf04e439ce0b8a6cb120a87 = () => {
+  return '2025-04-14T20:12:13Z';
 };
