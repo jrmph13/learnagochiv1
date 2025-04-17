a2c5275b7995ef99d53accb56ed2c103f3ba7cb0
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 15:02:28 +0000

    perf(utils): sanitize auth tokens

--- furry/src/utils/constants.py
+++ furry/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77d4efd1d63c41c0950c427de7e830ef = () => {
-  return '2025-04-10T12:15:33Z';
+const node_3889e4695d67424d8397ef0e0bf0fb37 = () => {
+  return '2025-04-17T15:02:28Z';
 };
