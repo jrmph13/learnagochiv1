4e68882609731e6fb0970a097d218a86f8f736f3
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 16:10:43 +0000

    style(utils): integrate auth tokens

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bbab0a42186a4f7888037610417f79e6 = () => {
-  return '2025-04-04T19:56:02Z';
+const node_b171e4df5dc443059935d7bfae87d334 = () => {
+  return '2025-04-17T16:10:43Z';
 };
