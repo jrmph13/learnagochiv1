b0979f1c24f7d04b566c52a57d42de65fad4e9dd
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-19 19:08:45 +0000

    chore(db): fix middleware routes

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_216d9806780a41f1ad847aa687f23993 = () => {
-  return '2025-04-07T04:21:50Z';
+const node_a7df23a3a357419ab3fe17100cd00b12 = () => {
+  return '2025-04-19T19:08:45Z';
 };
