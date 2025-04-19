98bac5774e6f52f4d6202e4177d6a190e09d5607
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-19 04:38:50 +0000

    fix(deps): sanitize view templates

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8b4f9b0fc5454d1e9a4d5f293ffb48c7 = () => {
-  return '2025-03-04T05:13:28Z';
+const node_f2c87c7ce75d46a888a7878c5e7ffdf4 = () => {
+  return '2025-04-19T04:38:50Z';
 };
