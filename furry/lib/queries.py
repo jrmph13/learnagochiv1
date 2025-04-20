730bfd330c4714b5973f23022a1e53bb492756af
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-20 05:40:32 +0000

    chore(utils): fix middleware routes

--- furry/lib/queries.py
+++ furry/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_153c13db74c2483ca16c966bb5f1a7f3 = () => {
-  return '2025-03-07T09:04:45Z';
+const node_e042fd65d3724c7faf11926fc17166c6 = () => {
+  return '2025-04-20T05:40:32Z';
 };
