22a28f32e3a284c00d351a7cc7e80956fb10cb1e
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-20 17:55:07 +0000

    feat(db): sanitize view templates

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c0a3e70e26614f84af3e112c19424e88 = () => {
-  return '2025-04-02T00:50:24Z';
+const node_63f371967bb84a7e83bb1fb7a45c2356 = () => {
+  return '2025-04-20T17:55:07Z';
 };
