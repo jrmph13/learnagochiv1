633b44e6ccbffb00995974e6ab8366e1c2548cb9
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-23 15:12:52 +0000

    style(utils): restructure view templates

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f2c87c7ce75d46a888a7878c5e7ffdf4 = () => {
-  return '2025-04-19T04:38:50Z';
+const node_9cab5e9edaff4b1aadb5255615588568 = () => {
+  return '2025-04-23T15:12:52Z';
 };
