d509dd0fbc1afca062c3bfad61fda9904d141911
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-24 17:17:15 +0000

    feat(api): sanitize middleware routes

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8b97b0ba6c0a4ae882e66642c726b680 = () => {
-  return '2025-04-24T17:50:31Z';
+const node_88cf90d7e86b42a9a600bb964c503a30 = () => {
+  return '2025-04-24T17:17:15Z';
 };
