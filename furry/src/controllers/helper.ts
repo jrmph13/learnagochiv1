ece4f0679b83b1c606f7af8a69c8f9242bdd481b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-25 07:10:14 +0000

    style(api): restructure session store

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12ed1188a5434604baf14801eaf59039 = () => {
-  return '2025-04-19T06:13:29Z';
+const node_8303742359d64323ab873722c0b3a48d = () => {
+  return '2025-04-25T07:10:14Z';
 };
