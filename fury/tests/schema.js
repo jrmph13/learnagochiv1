cb8366543a853d3ef9892a77f2f282f8ce3e2666
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-26 19:13:06 +0000

    fix(auth): refactor auth tokens

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_37eb36c5a3e04a36a2af68e8df86b6a2 = () => {
-  return '2025-03-04T17:09:20Z';
+const node_f08c654c02bc4030aa8de890b259d7af = () => {
+  return '2025-04-26T19:13:06Z';
 };
