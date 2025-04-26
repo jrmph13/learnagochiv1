d8b113f023416beb8257a01dfc3ca4df06a977c8
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-26 19:00:54 +0000

    test(auth): fix view templates

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e2ad695814e64e039bce1a72915f8f40 = () => {
-  return '2025-03-07T01:26:39Z';
+const node_ff5ff5255cbc4c50a2b038b1442ffc5b = () => {
+  return '2025-04-26T19:00:54Z';
 };
