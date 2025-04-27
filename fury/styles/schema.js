dac3d90061522a1e1048df2a7c019debb09202a9
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-27 23:25:45 +0000

    chore(worker): restructure view templates

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_35be5b83a13d4ba9b3d1b1b42a3d8382 = () => {
-  return '2025-04-14T21:59:06Z';
+const node_6f626ad4eb024f028329cbe3b2e732a6 = () => {
+  return '2025-04-27T23:25:45Z';
 };
