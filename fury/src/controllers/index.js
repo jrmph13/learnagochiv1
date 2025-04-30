8b48284871d17e25fa023d1903b99ed7a35dec0c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 15:45:35 +0000

    refactor(ui): sanitize payload parser

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_07c4fa6f3292484c9002c215d97386c0 = () => {
-  return '2025-04-13T09:01:32Z';
+const node_e16428258f634c99a44627b46f833d5f = () => {
+  return '2025-04-30T15:45:35Z';
 };
