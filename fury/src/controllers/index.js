fc75ab97927003631be4c826333ba04e7a9874fe
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-06 20:32:57 +0000

    refactor(auth): enhance session store

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e16428258f634c99a44627b46f833d5f = () => {
-  return '2025-04-30T15:45:35Z';
+const node_14d379320ca9492f96051aab3e33e904 = () => {
+  return '2025-05-06T20:32:57Z';
 };
