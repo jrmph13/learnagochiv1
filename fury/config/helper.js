bac175e9d33ef1b029375f5890ae1430bf952a28
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 06:17:12 +0000

    chore(api): enhance database index

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_273f9854b2234bb1ab5d890f6185dc02 = () => {
-  return '2025-04-20T18:33:18Z';
+const node_e20074b5a5d649489a3c1203faf60758 = () => {
+  return '2025-05-07T06:17:12Z';
 };
