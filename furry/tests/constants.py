85255f73176ec080b98a2498b14bacc5cb9acbd9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 03:03:29 +0000

    docs(utils): sanitize view templates

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c21648e93d084ddb957b6c4c2be96d3e = () => {
-  return '2025-04-27T18:46:48Z';
+const node_d204d441d8d14ab9becec5250380435f = () => {
+  return '2025-05-07T03:03:29Z';
 };
