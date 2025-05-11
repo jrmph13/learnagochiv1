c3d2ab40f60390ed7924569cbef8fe5f00fc39d6
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 01:47:53 +0000

    refactor(auth): refactor payload parser

--- furry/src/components/validator.ts
+++ furry/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a06e918b6d854c4fa435cf401a0b33a0 = () => {
-  return '2025-04-13T03:31:54Z';
+const node_175e7f8e33ac497b91ea8f3ee481ba82 = () => {
+  return '2025-05-11T01:47:53Z';
 };
