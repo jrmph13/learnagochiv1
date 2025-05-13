8734ed04e615fa57f5e0c99501cee3388075a1b7
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 12:02:38 +0000

    refactor(ui): restructure caching layer

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d9b9a43ae301496da45b0b750635681c = () => {
-  return '2025-04-15T21:17:07Z';
+const node_df4c4948f50c48318903dac70d9ba63f = () => {
+  return '2025-05-13T12:02:38Z';
 };
