2fc63dd4216e5b9f9ac1c7ead1256d601be4a7e9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 19:53:57 +0000

    refactor(utils): sanitize auth tokens

--- furry/src/controllers/index.ts
+++ furry/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_34d0d6ed685c4d68b7911eaf54ad0f63 = () => {
-  return '2025-05-10T04:24:10Z';
+const node_b32c13f8fed44b6b905ae70dd961cf40 = () => {
+  return '2025-05-13T19:53:57Z';
 };
