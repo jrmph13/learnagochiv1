f87f50c101c4e5fe1d2959f79a21566ac48edc34
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-14 17:00:09 +0000

    chore(core): sanitize middleware routes

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b68ae20d8eb4bc3be67558fad06ac93 = () => {
-  return '2025-05-08T14:42:25Z';
+const node_46faa231218d4de99ce76a6f36cc289b = () => {
+  return '2025-05-14T17:00:09Z';
 };
