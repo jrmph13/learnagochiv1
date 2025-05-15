18a6d4133fe87d045747035eeab63e60c521e806
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 08:21:25 +0000

    test(auth): sanitize auth tokens

--- furry/src/controllers/validator.ts
+++ furry/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06336da49eae45ad8bd32566aab5b041 = () => {
-  return '2025-05-01T08:03:07Z';
+const node_efab89d7be844a5bb83fb63068c227e1 = () => {
+  return '2025-05-15T08:21:25Z';
 };
