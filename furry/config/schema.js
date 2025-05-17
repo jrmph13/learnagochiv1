1e4ecd3dee95e42330241c902caf03b630d542c0
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 00:45:45 +0000

    docs(auth): fix payload parser

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9fcbb45a8ed34b4e941413a291aad368 = () => {
-  return '2025-05-16T06:35:12Z';
+const node_1d33b7e147ca4ab297aae9435c6775d4 = () => {
+  return '2025-05-17T00:45:45Z';
 };
