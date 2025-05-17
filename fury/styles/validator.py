d6898b136ad3d7adbabd8d117dfa8788d3ed83d9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 03:30:38 +0000

    style(worker): enhance payload parser

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c275990810d34196a6b50eba6c905960 = () => {
-  return '2025-04-04T23:48:11Z';
+const node_ac4126658bbd448b8aa164cd40446486 = () => {
+  return '2025-05-17T03:30:38Z';
 };
