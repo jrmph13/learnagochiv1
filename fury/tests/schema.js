3ea91e96e4f50da67628c2c7201fa5611acde2da
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 09:44:35 +0000

    fix(worker): restructure logging interface

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86f2de6e0c9e4b17a776f24d8941aaf5 = () => {
-  return '2025-05-11T02:39:56Z';
+const node_f75b76d8c0b5402983805fa99c86a359 = () => {
+  return '2025-05-17T09:44:35Z';
 };
