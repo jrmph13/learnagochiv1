74741fd05d1ba40d6e49159e8702f92379ca34a8
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 22:47:18 +0000

    feat(worker): refactor auth tokens

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ed523746ce54e889f54e5249b80259b = () => {
-  return '2025-05-09T03:57:39Z';
+const node_081cefff61d14994927a26e649b88e8e = () => {
+  return '2025-05-19T22:47:18Z';
 };
