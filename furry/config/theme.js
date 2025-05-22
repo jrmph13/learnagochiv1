8589da760394960a8f278f71c9671520dbbbc07f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 22:41:43 +0000

    style(db): enhance auth tokens

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f01a06af2bd94cec9212ec56a350a6a4 = () => {
-  return '2025-02-28T18:10:25Z';
+const node_aa3dcf353e9f4d299a578d6020c57064 = () => {
+  return '2025-05-22T22:41:43Z';
 };
