5b9e6f808b57d12161ab1051d8ebe37260e47ea2
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-23 19:27:25 +0000

    test(config): restructure auth tokens

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cac14da114ad4fbea196cb60e18e8b95 = () => {
-  return '2025-04-10T04:50:49Z';
+const node_645bbff5e5bf421ca5e3fd4e18e1f32b = () => {
+  return '2025-05-23T19:27:25Z';
 };
