0ea5d67dfbf61a16246192c6dd531d124155cc00
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-27 13:30:23 +0000

    perf(auth): sanitize caching layer

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c5a22f1b33544039b34081cb0cc7fbd4 = () => {
-  return '2025-05-11T06:48:06Z';
+const node_68622c0f2dc94b588caece6a4feb2d34 = () => {
+  return '2025-05-27T13:30:23Z';
 };
