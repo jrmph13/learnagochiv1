5a056bcc0b4f92da57bd5b85a405a2fea55d4c19
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 12:06:25 +0000

    feat(ui): fix auth tokens

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ea4dc9b36ff44cf9816e52fd660733d = () => {
-  return '2025-04-25T00:36:37Z';
+const node_24d2d0b51957477c8754c6f2028ed4d8 = () => {
+  return '2025-05-28T12:06:25Z';
 };
