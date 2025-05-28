5f2aa98e555a9c3251db0da074a7ba6079b96a0d
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 18:04:28 +0000

    docs(config): sanitize view templates

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_028252024eb14fa89e30544e9026f5de = () => {
-  return '2025-05-21T10:34:03Z';
+const node_d60de2f87fd54a80821a1c8628deda6b = () => {
+  return '2025-05-28T18:04:28Z';
 };
