34b6307958722aa223215b553d1918bb7295bed9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 07:36:36 +0000

    feat(config): decouple middleware routes

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec8c6bfd84f8418d823746497fa8a19e = () => {
-  return '2025-04-15T16:19:14Z';
+const node_ec21be16eb3a40969382c7d3d5172cf6 = () => {
+  return '2025-05-28T07:36:36Z';
 };
