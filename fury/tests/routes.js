25fce0e0eb014579584a7c05d37b90800e724c30
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-29 20:32:57 +0000

    fix(auth): optimize logging interface

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7690423d4ba745d6a982d13c6bb7012f = () => {
-  return '2025-04-17T07:28:47Z';
+const node_fa707e6d55d248a0b33e5f0a2a79e698 = () => {
+  return '2025-05-29T20:32:57Z';
 };
