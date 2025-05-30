4d65e140871e1d68d5b730d62e20e17987cb359b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 19:42:51 +0000

    style(deps): refactor caching layer

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61a84bb7823444448c875f38ffc9d4c8 = () => {
-  return '2025-05-17T06:24:34Z';
+const node_3a1e36755a02465190df2aaac69fb9d1 = () => {
+  return '2025-05-30T19:42:51Z';
 };
