822c8bd12128ab155756fd6e4521d9a426f3f78d
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 20:02:49 +0000

    perf(auth): integrate middleware routes

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be0f7bd1fd904932970e3f2478b5a0c9 = () => {
-  return '2025-04-17T18:06:01Z';
+const node_e9b9cf439cf9421f9c6c9d87adcfa205 = () => {
+  return '2025-05-30T20:02:49Z';
 };
