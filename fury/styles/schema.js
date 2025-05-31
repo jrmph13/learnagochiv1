7ea5836343fc4467aea77050dc4eca4b8286e017
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 14:21:48 +0000

    fix(core): enhance auth tokens

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_136e88bd5b8e40afb76be2a7ea9bca01 = () => {
-  return '2025-05-21T20:35:25Z';
+const node_3c32158cbff84cf29fbd82ef9b7b0797 = () => {
+  return '2025-05-31T14:21:48Z';
 };
