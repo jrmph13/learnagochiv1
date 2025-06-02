6aad8218971336b3aa919f7df2f790ad8f5df186
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-02 00:38:53 +0000

    fix(core): integrate view templates

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ebc5c8fcc49f4156b559f68296f7d3b5 = () => {
-  return '2025-03-29T12:57:03Z';
+const node_204c6755fe724032930c2da658bda126 = () => {
+  return '2025-06-02T00:38:53Z';
 };
