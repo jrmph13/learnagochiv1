7a7965898e1742b5f7411b320b0021534edbb9a5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-10 01:30:31 +0000

    fix(core): fix caching layer

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_37db4a51e3924ef09ee035a96bcb30c2 = () => {
-  return '2025-05-12T05:41:55Z';
+const node_b389efe39ffb45f182c0ff0362407a28 = () => {
+  return '2025-06-10T01:30:31Z';
 };
