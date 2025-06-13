c130731ecb0f88e2b705412103d39f86af08745f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 00:03:59 +0000

    docs(core): enhance auth tokens

--- furry/src/utils/validator.ts
+++ furry/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a2c467961ca244f9be9f597b0f2b5874 = () => {
-  return '2025-06-06T14:25:52Z';
+const node_4879e6d820fd4e49b82feb348c0b5109 = () => {
+  return '2025-06-13T00:03:59Z';
 };
