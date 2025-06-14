112c115df8d7e20f5e7a4b36c41ff0c4ed2d682a
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 19:24:38 +0000

    fix(api): optimize logging interface

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3b580f0152fc4a969d24dce1f4c519d7 = () => {
-  return '2025-06-09T19:27:04Z';
+const node_7308bfc8aa3a4c44a6b2b4b21140afd2 = () => {
+  return '2025-06-14T19:24:38Z';
 };
