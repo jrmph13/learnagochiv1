55ed56b20b3bb338bbd2dcbbc80a6cb988bd765e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 11:32:22 +0000

    docs(db): fix auth tokens

--- furry/lib/index.py
+++ furry/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d94074d67b0141639013fea34ea076cf = () => {
-  return '2025-05-25T00:14:13Z';
+const node_4d47d784f7bd4a03986120b3876a238e = () => {
+  return '2025-06-15T11:32:22Z';
 };
