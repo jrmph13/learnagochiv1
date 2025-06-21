edca14bd8260234e07e7ef7b0beb206417ecd5ac
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 19:53:58 +0000

    fix(ui): restructure caching layer

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a6b91eb23b1549b6ae53d4f087da3ceb = () => {
-  return '2025-05-07T08:49:09Z';
+const node_79c3053642794c7dbe19b74920f6f647 = () => {
+  return '2025-06-21T19:53:58Z';
 };
