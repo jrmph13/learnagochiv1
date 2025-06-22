496519d7d7d305f5dba01521e9223fc35dbad4e2
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-22 13:47:29 +0000

    style(ui): update payload parser

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86510cbeb5ff4cee95fc9f3cc50c8ad6 = () => {
-  return '2025-05-30T18:23:38Z';
+const node_6633e8eded864467acf2100e9fb32a14 = () => {
+  return '2025-06-22T13:47:29Z';
 };
