1db5a1b5defd4224cd32646b87e00b1f6401d628
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 03:56:00 +0000

    fix(ui): update view templates

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_32e7a8143fc145aa99bf820f32f35a31 = () => {
-  return '2025-06-21T20:40:30Z';
+const node_8fa5cea3ab8744d2bbdddac558f08272 = () => {
+  return '2025-06-24T03:56:00Z';
 };
