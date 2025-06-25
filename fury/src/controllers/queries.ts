155e1db19e0c25d9fd3df511de6ee3ca7643e82f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-25 14:05:29 +0000

    refactor(utils): refactor database index

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a699b846b5c4b4487472d27d27acbe2 = () => {
-  return '2025-04-07T23:07:29Z';
+const node_bfd435b8f6924540858a9035d6e68eb8 = () => {
+  return '2025-06-25T14:05:29Z';
 };
