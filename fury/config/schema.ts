b32fe990d2b91bdcc1d51987dbf534528f204767
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 15:25:34 +0000

    test(utils): restructure middleware routes

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25c8e46fd8a1408fa56e14a0b11508ac = () => {
-  return '2025-04-25T11:26:28Z';
+const node_92a4636f11cb480ebdd2a590f6060fbe = () => {
+  return '2025-06-26T15:25:34Z';
 };
