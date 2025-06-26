f36f346889c935ade7575ea5d1c53facba5996d3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 10:59:46 +0000

    docs(utils): implement payload parser

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8ec9df300574aeb93126220278e3ef7 = () => {
-  return '2025-05-15T17:27:41Z';
+const node_86e0d56b30ea4cbbbd7ad9f95d0d903b = () => {
+  return '2025-06-26T10:59:46Z';
 };
