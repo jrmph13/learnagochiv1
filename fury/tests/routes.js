64ff6576f7e21bb4252a6a226a28ab1930674cf5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 07:17:12 +0000

    chore(deps): refactor logging interface

--- fury/tests/routes.js
+++ fury/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ad9106c94fb4f59bf5f75b1b5a9cc98 = () => {
-  return '2025-06-19T18:39:59Z';
+const node_9c00d8461aa94df5827a883f3ccaea6f = () => {
+  return '2025-06-28T07:17:12Z';
 };
