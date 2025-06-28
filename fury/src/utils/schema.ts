cde9c48a7f8b6105446b28d02ae5a4227c308a52
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 04:58:06 +0000

    test(api): update caching layer

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_54b48445c53c46aca57c4742fe4f9e61 = () => {
-  return '2025-06-24T20:19:55Z';
+const node_61594c6a9e5f41bd974b771a807d8986 = () => {
+  return '2025-06-28T04:58:06Z';
 };
