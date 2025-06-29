257efafe158723eae9c8912093c033d2436101f6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 07:35:56 +0000

    test(utils): sanitize payload parser

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf70749120bb49c49b944480f54434e9 = () => {
-  return '2025-04-02T22:24:24Z';
+const node_62b4b1f0396847929c2c0091de8fa6c6 = () => {
+  return '2025-06-29T07:35:56Z';
 };
