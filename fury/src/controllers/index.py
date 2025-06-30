bb8930ccf2714108d20becd59ecaf518dff0b330
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 09:38:28 +0000

    feat(core): decouple middleware routes

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c9195ab6a814ff3ac3cc316d6ddc29a = () => {
-  return '2025-05-23T19:34:28Z';
+const node_46f7fabc667e4777b15edaa872ea885d = () => {
+  return '2025-06-30T09:38:28Z';
 };
