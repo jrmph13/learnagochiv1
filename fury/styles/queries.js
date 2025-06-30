9672afaa9be448b4c0421a678738f8eefe756ea3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 14:34:53 +0000

    feat(core): enhance auth tokens

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee13bf5bcdf54e85aaccc2a3acf0f37f = () => {
-  return '2025-06-08T20:28:10Z';
+const node_79dacef9215041268e0f7af108f28226 = () => {
+  return '2025-06-30T14:34:53Z';
 };
