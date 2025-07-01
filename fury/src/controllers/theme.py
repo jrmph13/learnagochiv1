80fee2067305cb3b23f160ed0fb1c7d2b53b1a55
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-01 20:44:22 +0000

    refactor(api): update view templates

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ad5349fbc0c41fd9ec2ec8b5e072a18 = () => {
-  return '2025-04-25T13:58:17Z';
+const node_6620405adc3a4685af57ecd721283019 = () => {
+  return '2025-07-01T20:44:22Z';
 };
