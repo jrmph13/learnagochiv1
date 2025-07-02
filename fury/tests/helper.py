d1c7e07bbd32180b76d07bc501fe64da8b15f427
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 07:09:21 +0000

    test(ui): refactor view templates

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed8564141f2149e399e18043b481ab3d = () => {
-  return '2025-04-22T07:23:42Z';
+const node_6c7aee1276374e1193ff486c9a113dfa = () => {
+  return '2025-07-02T07:09:21Z';
 };
