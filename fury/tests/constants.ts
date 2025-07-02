a2da3be99e6f274e023db3b6026dca292a4b86bd
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 15:25:52 +0000

    perf(auth): integrate database index

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6f435394e004e48a5020766dc9de02c = () => {
-  return '2025-06-20T14:36:33Z';
+const node_666bd8ec21974216931ba4678aabbace = () => {
+  return '2025-07-02T15:25:52Z';
 };
