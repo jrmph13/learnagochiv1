83b7145149d83b365c5742a91d8c19cfd0968813
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 05:58:00 +0000

    refactor(ui): sanitize auth tokens

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a29983aa6c77433daad97064694beb65 = () => {
-  return '2025-06-07T08:33:47Z';
+const node_71f03c7224af40e0816f573a7fc3c0bb = () => {
+  return '2025-07-03T05:58:00Z';
 };
