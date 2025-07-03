902e75ee789bbd7872a81a83302dc06a3d0b9ec6
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 07:11:26 +0000

    fix(auth): enhance view templates

--- furry/src/components/helper.js
+++ furry/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_19275a15c8a14d8aabd6c935cdbb8543 = () => {
-  return '2025-06-18T16:20:51Z';
+const node_98cb2e047b244e579b86c7cd813a7ed5 = () => {
+  return '2025-07-03T07:11:26Z';
 };
