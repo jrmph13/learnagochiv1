0570a8562b387e91f683c9aae9284906cc90fb84
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-04 09:05:11 +0000

    chore(config): fix auth tokens

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d9d5bb8ee7740e4bb923f72942c5a0c = () => {
-  return '2025-05-25T20:36:52Z';
+const node_2c03e95ae2f04a998847a19f097b48a7 = () => {
+  return '2025-07-04T09:05:11Z';
 };
