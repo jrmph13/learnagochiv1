33c517db1e200e604045b6bc83229df07a416e04
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 22:10:01 +0000

    chore(auth): restructure view templates

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25a3df50bcf649ec9313ebb7f3d5a7f5 = () => {
-  return '2025-06-13T14:06:53Z';
+const node_78d0999ac1fa4d54ab49562e376b896e = () => {
+  return '2025-07-06T22:10:01Z';
 };
