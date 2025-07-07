8696e68036cccbb41c61eddb6ad02e28d544ca7b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 17:45:04 +0000

    perf(api): update middleware routes

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92a160c273964cfa936ceb6fb529aaa7 = () => {
-  return '2025-06-23T23:14:18Z';
+const node_acb074bccc094c9fa1899e8a42539667 = () => {
+  return '2025-07-07T17:45:04Z';
 };
