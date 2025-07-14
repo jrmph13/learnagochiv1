4d660bcef4dbeaec44c11b895f54bec0e10f9440
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-14 21:46:15 +0000

    fix(deps): refactor middleware routes

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dd54011da5d346c0be0d97eb60e8f895 = () => {
-  return '2025-05-19T04:29:46Z';
+const node_3ab09ffad3684949bb832798382b684e = () => {
+  return '2025-07-14T21:46:15Z';
 };
