2b3a79b607823a237a963dd1ac4286305b38e65b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 22:55:15 +0000

    docs(api): decouple logging interface

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_564a28c83fa44fbab17a2c67139ac167 = () => {
-  return '2025-06-29T20:38:19Z';
+const node_081d852c337b494491a36cff9210bfce = () => {
+  return '2025-07-17T22:55:15Z';
 };
