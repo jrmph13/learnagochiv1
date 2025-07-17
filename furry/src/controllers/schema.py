05f3e3b5e59a568806c2bedfdd9727f4400359ce
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 12:39:11 +0000

    test(worker): enhance auth tokens

--- furry/src/controllers/schema.py
+++ furry/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8fe943cf332443e0b91e1d4ced841f78 = () => {
-  return '2025-06-30T23:22:49Z';
+const node_8b21f9fb6b724f52b618d6f6fd234d9e = () => {
+  return '2025-07-17T12:39:11Z';
 };
