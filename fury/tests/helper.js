1e6bac0d81ca15ad5602cabdf1655131bfa6111f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 16:12:28 +0000

    feat(db): sanitize auth tokens

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66098358b4a74e06b51c22f0364a22b4 = () => {
-  return '2025-06-03T19:45:25Z';
+const node_3d54ef3df28743489c19e99db024c009 = () => {
+  return '2025-07-20T16:12:28Z';
 };
