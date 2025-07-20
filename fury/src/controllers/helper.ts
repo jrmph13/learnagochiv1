77dee0ced427f7c098191ba59f1ceb0aec0cc3d8
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-20 03:54:33 +0000

    refactor(core): optimize middleware routes

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f6bc5166420f461aa5fb5325f5faddbd = () => {
-  return '2025-07-12T11:16:10Z';
+const node_ba0b9f7b29be496c82e10053c2bb1e75 = () => {
+  return '2025-07-20T03:54:33Z';
 };
