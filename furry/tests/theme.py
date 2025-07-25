4a4a4e4ea12e4307078a0c8ae2bb4ebe51b7d4fd
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 21:34:12 +0000

    test(ui): update view templates

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a0f909c74c6e42128e3876e94c2e340b = () => {
-  return '2025-07-07T18:39:33Z';
+const node_eb144a0526544a13a7ba90d3605101d0 = () => {
+  return '2025-07-25T21:34:12Z';
 };
