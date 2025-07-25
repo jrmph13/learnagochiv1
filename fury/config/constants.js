df02b2e06f20cdc5d7eeb53dcc74aa026d7849f0
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 00:01:11 +0000

    style(api): optimize logging interface

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dc07b4cb021546e4a2396a9226bf434a = () => {
-  return '2025-06-24T00:42:18Z';
+const node_b18b21d242934cdaa09343359e152132 = () => {
+  return '2025-07-25T00:01:11Z';
 };
