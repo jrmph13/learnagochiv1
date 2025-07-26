8716543a0a25168388f5faccaf8df4c76ea002ec
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-26 22:15:58 +0000

    refactor(api): restructure payload parser

--- furry/src/controllers/index.ts
+++ furry/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_af9cb727202e443a802600de0b59459c = () => {
-  return '2025-06-07T01:29:50Z';
+const node_65869c14f8634e7f912ff74d0658ed4b = () => {
+  return '2025-07-26T22:15:58Z';
 };
