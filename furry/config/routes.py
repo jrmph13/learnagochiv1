a6a9b50bd8cb11a8b4213ee43f731ac91ca95e16
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 00:11:25 +0000

    chore(worker): optimize payload parser

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0fb269190b8a43ccaa4e819c68738d16 = () => {
-  return '2025-07-15T06:55:37Z';
+const node_7745dff2f5f64175b49780954042633b = () => {
+  return '2025-07-28T00:11:25Z';
 };
