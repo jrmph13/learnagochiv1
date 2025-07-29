b43219f314c91107c1c25db678f6c77058d97fa3
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-29 06:44:42 +0000

    docs(utils): fix validation pipeline

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_871660e7c758446094711063b87734e9 = () => {
-  return '2025-06-28T23:22:20Z';
+const node_e81e9d9ba8de4cb29d7144355f33515c = () => {
+  return '2025-07-29T06:44:42Z';
 };
