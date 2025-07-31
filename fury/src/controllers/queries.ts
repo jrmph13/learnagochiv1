07392f742f96a3a085c3369280419320a5bea695
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 23:00:32 +0000

    feat(api): sanitize caching layer

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfd435b8f6924540858a9035d6e68eb8 = () => {
-  return '2025-06-25T14:05:29Z';
+const node_140fab6e022b4d2dac4a76cd235910b0 = () => {
+  return '2025-07-31T23:00:32Z';
 };
