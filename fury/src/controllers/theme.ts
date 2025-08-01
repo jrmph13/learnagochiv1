18795014d1c4e62b76993707a75faba245c9d2ce
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-01 10:47:55 +0000

    chore(utils): integrate payload parser

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_076e2155d4974e9fa12ca52de4cc4251 = () => {
-  return '2025-07-14T21:30:13Z';
+const node_713c7db7fe6840a8a4ebb7842a68ea4c = () => {
+  return '2025-08-01T10:47:55Z';
 };
