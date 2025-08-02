87696c28947c2f0fd35deb6e0c69264950e337df
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 15:39:22 +0000

    fix(worker): enhance logging interface

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_42e6f42dedb544c3bd83ac1f8e17ba8a = () => {
-  return '2025-05-08T21:46:54Z';
+const node_7cb725b5cb92435d99b609e1e309753a = () => {
+  return '2025-08-02T15:39:22Z';
 };
