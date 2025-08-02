f467270ab694570520d4f6b301c66ff4f164bbbc
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 05:46:29 +0000

    chore(config): sanitize view templates

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f9dfbe2b3b4d49e29de5a025a5fcaa32 = () => {
-  return '2025-06-18T18:55:37Z';
+const node_c4c5f4dce55640c0a2248d9d4b8e0b1a = () => {
+  return '2025-08-02T05:46:29Z';
 };
