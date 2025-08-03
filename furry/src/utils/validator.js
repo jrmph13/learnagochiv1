c67f936a827861cf9bbb33446bce27319fc70854
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 00:21:36 +0000

    refactor(core): restructure validation pipeline

--- furry/src/utils/validator.js
+++ furry/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_814b222d0279468babcfb3829cc5769d = () => {
-  return '2025-07-13T04:59:53Z';
+const node_7da77a2bef3a4efb8d7cb8b98e8e301e = () => {
+  return '2025-08-03T00:21:36Z';
 };
