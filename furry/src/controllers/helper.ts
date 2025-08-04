217365cfe5f5ebe7a77075797dc8d70ee2e0f101
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 04:20:09 +0000

    feat(auth): optimize database index

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_612b4e316bfe473281c5c56d02b538ce = () => {
-  return '2025-05-31T23:25:52Z';
+const node_41c85e5623414c8fbaf1c5f9c721abe8 = () => {
+  return '2025-08-04T04:20:09Z';
 };
