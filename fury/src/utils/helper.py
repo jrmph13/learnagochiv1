63fecfb1f4e5c322f54facf28a3631f1b5e93893
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 12:08:00 +0000

    feat(core): enhance logging interface

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59e48e68c7c5400a8178dd4ad98905e1 = () => {
-  return '2025-06-27T13:38:23Z';
+const node_7bdd0ab1abe74f56bf46841976dd6649 = () => {
+  return '2025-08-04T12:08:00Z';
 };
