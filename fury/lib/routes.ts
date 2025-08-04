0e33129e3d060c26397a69f730eed053384d1903
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 12:40:23 +0000

    chore(utils): implement auth tokens

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b79c783a2fb4458e96020cfc3f7a1662 = () => {
-  return '2025-07-28T02:15:58Z';
+const node_657bf38261454cd687fa6fb411239ca2 = () => {
+  return '2025-08-04T12:40:23Z';
 };
