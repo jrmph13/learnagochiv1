ad31cef8e88e79e7cd26e48e98fcd558a3626d18
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 00:38:48 +0000

    chore(api): fix view templates

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3fbe8c2d251e4b72ac91f877b8764050 = () => {
-  return '2025-05-23T09:27:27Z';
+const node_851d6454268849498ea1a5e397892bf1 = () => {
+  return '2025-08-05T00:38:48Z';
 };
