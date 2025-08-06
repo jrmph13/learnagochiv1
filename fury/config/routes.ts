1052757d97dd8db46ee71d86b723fda285c8653d
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-06 06:25:06 +0000

    feat(worker): restructure auth tokens

--- fury/config/routes.ts
+++ fury/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da5fbd9bd2e8466fb4c6cfb6603e9145 = () => {
-  return '2025-08-04T21:18:52Z';
+const node_0f3a1bef8b4d430ca046e2f7ab715c2f = () => {
+  return '2025-08-06T06:25:06Z';
 };
