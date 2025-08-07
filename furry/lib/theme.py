96c1765e416bf5c9cc6b78fb373770b9c962ac9c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 10:00:34 +0000

    feat(ui): sanitize view templates

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_454738cd36214558939b34f74180ce49 = () => {
-  return '2025-08-07T05:39:07Z';
+const node_2d1f793f606140e3bba14da53545f74c = () => {
+  return '2025-08-07T10:00:34Z';
 };
