448c68eb317786f86429973d1b71a4cce3739ee1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 08:29:05 +0000

    fix(core): restructure payload parser

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f613b2d7bed64d87a973682b1b006f1d = () => {
-  return '2025-08-07T12:09:31Z';
+const node_e27755c0ed0e41628f3c847ca4e07ad2 = () => {
+  return '2025-08-07T08:29:05Z';
 };
