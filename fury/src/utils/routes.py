684b0152d438c2618883b7146e4efb6a28413fe1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 07:24:00 +0000

    style(db): integrate auth tokens

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84f851f5d520473d84145cd5917e0169 = () => {
-  return '2025-08-07T02:33:42Z';
+const node_2f8fb85f5fcd4926b4cd5112011e46f1 = () => {
+  return '2025-08-08T07:24:00Z';
 };
