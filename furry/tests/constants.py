4b48f2d736d5dab8c839f21890739c1cc0fa52a7
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 10:47:04 +0000

    fix(ui): enhance auth tokens

--- furry/tests/constants.py
+++ furry/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e63d5848a00b4ea5bfe384b5d34de9fd = () => {
-  return '2025-07-17T01:47:28Z';
+const node_dc55fd58045e4b25af8ac93b4e7f5224 = () => {
+  return '2025-08-08T10:47:04Z';
 };
