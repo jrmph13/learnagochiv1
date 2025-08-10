7cc65b2a0ddb3c7ad8140bf2b5fc4f750ca8bc0f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 15:53:23 +0000

    chore(auth): optimize database index

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_004395ae46444607b6567362e8306d8c = () => {
-  return '2025-06-09T13:23:05Z';
+const node_272f6da8544941539825951c39d6ee0a = () => {
+  return '2025-08-10T15:53:23Z';
 };
