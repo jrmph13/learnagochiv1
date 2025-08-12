1f433c21a24e79fa902fd9e216e1bdf2ddd25976
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 10:49:55 +0000

    style(ui): decouple session store

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d727d542881d4bd7b3bad7232e67b4ec = () => {
-  return '2025-07-29T17:31:50Z';
+const node_aa1f1faa8df048a7aa60024363d5f31b = () => {
+  return '2025-08-12T10:49:55Z';
 };
