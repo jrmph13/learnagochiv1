1561acfd05a54ec1281dc065b12720946348a996
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 00:32:40 +0000

    refactor(worker): sanitize logging interface

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bf183709ba94337bdad3d5e63599224 = () => {
-  return '2025-07-30T08:46:04Z';
+const node_60121b7f668448dbb5a8c790fe0d6845 = () => {
+  return '2025-08-15T00:32:40Z';
 };
