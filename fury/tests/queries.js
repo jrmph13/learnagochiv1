5edc528646c589de65e95a7ef6c2c5407a7fc6c7
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-18 15:14:57 +0000

    chore(worker): integrate payload parser

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc13d37d20df414a90c52ebd3b81b91d = () => {
-  return '2025-07-25T15:06:46Z';
+const node_696d7dd8e2b34eba803d7b03b6991bef = () => {
+  return '2025-08-18T15:14:57Z';
 };
