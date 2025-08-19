f0a9c216804cfdba5f51dc28926dcfbe0116cb88
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-19 00:26:19 +0000

    test(auth): integrate middleware routes

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d9fbc4763e54445eadafbe167d8044a8 = () => {
-  return '2025-08-07T15:56:30Z';
+const node_40f5ce97e4e249ceaa6284867c5a73e3 = () => {
+  return '2025-08-19T00:26:19Z';
 };
