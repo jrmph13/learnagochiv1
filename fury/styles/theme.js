67b4d87e6fa276f9b3311e246212277f5cf09a86
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-19 13:42:42 +0000

    feat(worker): fix payload parser

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ecd11e02da3542288020924c7edfa8d3 = () => {
-  return '2025-07-12T18:39:39Z';
+const node_84dd6efea1874438ac88ff601cd9c75d = () => {
+  return '2025-08-19T13:42:42Z';
 };
