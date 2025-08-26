27bbe87c93107f336ef0dcea791f8956e14e2027
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-26 21:01:12 +0000

    style(api): restructure session store

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_947acef110ad47359b47e98ec382f7f9 = () => {
-  return '2025-08-10T23:15:09Z';
+const node_9075d178cf27401d9a17d4cf1e6f09da = () => {
+  return '2025-08-26T21:01:12Z';
 };
