185cf59f079b7e85564ca0ec9a2efe802663d23f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-29 15:49:19 +0000

    docs(db): fix view templates

--- furry/src/controllers/routes.js
+++ furry/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8876c8c8ad6c4ba08cc0b4856f75d33e = () => {
-  return '2025-08-02T18:06:57Z';
+const node_9b289ca155684673af7cc0e534b2390a = () => {
+  return '2025-08-29T15:49:19Z';
 };
