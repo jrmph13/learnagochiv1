ddb3d362b62ba047031eca4fddb2fe59b7f37dca
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-30 03:30:10 +0000

    style(worker): refactor middleware routes

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_55a0ba027c7346fbb887ca18d2c0ce3e = () => {
-  return '2025-08-24T07:54:56Z';
+const node_3ad4a8b36c43443bb26f79961a0f698c = () => {
+  return '2025-08-30T03:30:10Z';
 };
