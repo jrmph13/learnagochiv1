e59389d7167fe05f6184642e0f117b468bd7768d
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-08 04:25:14 +0000

    test(worker): restructure auth tokens

--- furry/src/components/routes.js
+++ furry/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_08f0ea14463548aa9294a249d6c4ade3 = () => {
-  return '2025-08-04T01:40:54Z';
+const node_99311c20e431467cb4dd12a43277a66b = () => {
+  return '2025-09-08T04:25:14Z';
 };
