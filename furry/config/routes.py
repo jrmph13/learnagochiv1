45a905106baa19101fbc1f2cf1492c8ec59fb5d4
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-10 06:23:08 +0000

    refactor(auth): implement payload parser

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7745dff2f5f64175b49780954042633b = () => {
-  return '2025-07-28T00:11:25Z';
+const node_98850df62dd44630b00f933f4628d73e = () => {
+  return '2025-09-10T06:23:08Z';
 };
