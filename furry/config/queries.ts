ff206f82656a6a2ec48296996cb64e311ca72a0a
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-10 07:20:08 +0000

    fix(ui): enhance database index

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a0968fd6a8cb4a44ace61d05aac260ba = () => {
-  return '2025-09-04T09:59:15Z';
+const node_9698cd0fc0b240b7880c682ce61a18fc = () => {
+  return '2025-09-10T07:20:08Z';
 };
