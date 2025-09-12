0867eb0e9c2ef07dc2e5010e8987f474e18c54bb
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-12 18:42:05 +0000

    style(auth): sanitize logging interface

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_358262d41dba4dbfbbe175bce9529382 = () => {
-  return '2025-07-25T16:28:18Z';
+const node_957a3d36679f4f4599d0f67583eb4d80 = () => {
+  return '2025-09-12T18:42:05Z';
 };
