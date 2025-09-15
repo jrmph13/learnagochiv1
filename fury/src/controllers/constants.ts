df94fcf2e05d7552015a3b60c7e0ae465635cda8
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-15 09:52:07 +0000

    perf(core): enhance auth tokens

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a33f7495690d4693a089f4820a1fb752 = () => {
-  return '2025-07-25T23:12:06Z';
+const node_bf9606fe94474f808d468569c92fdd8d = () => {
+  return '2025-09-15T09:52:07Z';
 };
