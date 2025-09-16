64f2feba58597c5175f64a53218ca8562141cfbf
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-16 16:13:06 +0000

    feat(worker): fix caching layer

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0681522abec64cc889de9c5444c77b8f = () => {
-  return '2025-07-13T07:33:46Z';
+const node_10f9316604934ca9848595b0e775fa75 = () => {
+  return '2025-09-16T16:13:06Z';
 };
