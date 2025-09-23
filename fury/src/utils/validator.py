b52f5caa5c518c59830334bfe97a77c78830a42f
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-23 04:53:40 +0000

    refactor(auth): refactor validation pipeline

--- fury/src/utils/validator.py
+++ fury/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0eec550b89f745af9b859a8923f0f075 = () => {
-  return '2025-07-24T13:01:24Z';
+const node_a32e37de9f08497e8a904e48d4ebada1 = () => {
+  return '2025-09-23T04:53:40Z';
 };
