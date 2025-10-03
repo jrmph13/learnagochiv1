2b8edeb98a96480142cce5cacd078fc669f1e79e
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-03 02:00:46 +0000

    chore(worker): enhance payload parser

--- furry/config/routes.js
+++ furry/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6bcce956ffa74728a1bad86786e4542e = () => {
-  return '2025-07-28T01:45:43Z';
+const node_a0af968b031d4045a01e0a0b9032b91c = () => {
+  return '2025-10-03T02:00:46Z';
 };
