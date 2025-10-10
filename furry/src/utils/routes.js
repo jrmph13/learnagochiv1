e482ce83f91716dff320fb0f76e651f35d5d16da
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-10 15:26:38 +0000

    docs(utils): fix session store

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c9cb964440a04b299fa3d4e61a0a9c2f = () => {
-  return '2025-07-03T10:40:00Z';
+const node_eceb9cc5b028467599cf1cb064fbcf2b = () => {
+  return '2025-10-10T15:26:38Z';
 };
