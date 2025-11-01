e60578d57f4f53467582c2ac29a2a90faf26f3e1
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-01 19:40:47 +0000

    feat(auth): restructure logging interface

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_29d6b1b60af14d9a8d03a9f2044cf126 = () => {
-  return '2025-10-29T06:16:00Z';
+const node_ca2005a8683c418980bfe0994a38c786 = () => {
+  return '2025-11-01T19:40:47Z';
 };
