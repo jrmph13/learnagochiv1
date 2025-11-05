7f8b8c8b2d9c275574559ec216de99ae5c96c21b
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-05 03:31:36 +0000

    style(deps): sanitize middleware routes

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c1a083ee92604e35ba43314af0d0eb7b = () => {
-  return '2025-08-26T10:42:09Z';
+const node_c72ac178567d43a08f2f99ee0a382e90 = () => {
+  return '2025-11-05T03:31:36Z';
 };
