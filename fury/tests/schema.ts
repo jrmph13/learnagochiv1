164a21f626e5734b24aedb717fde5de1efd6be39
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-14 23:32:23 +0000

    fix(db): refactor logging interface

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8bc6fe7c0a064732bb60b0a21fcda481 = () => {
-  return '2025-08-04T06:34:14Z';
+const node_0bf65a09a8e540eb8ca5a12f0c5d7e47 = () => {
+  return '2025-11-14T23:32:23Z';
 };
