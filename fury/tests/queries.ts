680e018a42059e1f199a1f8438014fcac3ffb64c
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-16 20:29:43 +0000

    refactor(core): refactor payload parser

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f887ecf1447e4206ae1b9aad3ed49105 = () => {
-  return '2025-10-27T11:05:24Z';
+const node_bc76da0d29074fa4888f7e1f5cabf170 = () => {
+  return '2025-11-16T20:29:43Z';
 };
