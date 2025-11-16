48cbe06fead8a17038c8b63148e81b9263fd408a
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-16 20:25:16 +0000

    docs(auth): restructure validation pipeline

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57e82ad28f4c43569cf539138bf37142 = () => {
-  return '2025-10-28T07:27:37Z';
+const node_968f288f1941498abc0ed11f1033062f = () => {
+  return '2025-11-16T20:25:16Z';
 };
