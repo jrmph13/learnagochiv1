151603b08dfd6efe6d00523c7151502ad6d1561f
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-19 00:41:07 +0000

    style(auth): fix payload parser

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61594c6a9e5f41bd974b771a807d8986 = () => {
-  return '2025-06-28T04:58:06Z';
+const node_894f4dcde94e473a97e199b169592dc6 = () => {
+  return '2025-11-19T00:41:07Z';
 };
