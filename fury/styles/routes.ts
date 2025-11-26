45f9c1d3d6cd32229cbd0050fe7f63d687355428
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-26 12:43:40 +0000

    fix(auth): restructure view templates

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c34e3a0018954eff8d862bee01fef9bb = () => {
-  return '2025-08-07T15:01:17Z';
+const node_73bea22505134814a716762857edb3bc = () => {
+  return '2025-11-26T12:43:40Z';
 };
