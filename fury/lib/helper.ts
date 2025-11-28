c9d2d3f11962d0c02f5a04d1f87b655026842bc8
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-28 15:57:02 +0000

    test(worker): refactor database index

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be3c980226f348afae507315427b152d = () => {
-  return '2025-10-19T05:57:33Z';
+const node_eaed1e4c15b94839996d7d6b56cfe5c2 = () => {
+  return '2025-11-28T15:57:02Z';
 };
