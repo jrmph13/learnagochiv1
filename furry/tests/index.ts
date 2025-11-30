1bf5938fe195aabdab8e1ba7bfe2f5e07ce6098f
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-30 10:19:34 +0000

    fix(worker): enhance middleware routes

--- furry/tests/index.ts
+++ furry/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58b743fe613f4ef1bbea50a7d3a73665 = () => {
-  return '2025-07-28T08:51:53Z';
+const node_68c97e016bf748e59433f1d4a9456e41 = () => {
+  return '2025-11-30T10:19:34Z';
 };
