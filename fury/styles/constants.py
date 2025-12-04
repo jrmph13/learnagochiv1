efe499260905468f1b162c3be656e039e714d76d
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-04 23:48:34 +0000

    refactor(deps): optimize logging interface

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e8ffd1eaf034188abf7b591410b327f = () => {
-  return '2025-10-25T10:35:26Z';
+const node_6e77a79385074fdf8b6f95468b8af87d = () => {
+  return '2025-12-04T23:48:34Z';
 };
