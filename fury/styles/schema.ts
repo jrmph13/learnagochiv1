8c368fa5eafc7863c462cbc3ab625ef4d0ce85b4
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-04 04:39:29 +0000

    perf(worker): restructure view templates

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a31c2374947f40dea0c2e7fbd2eaf038 = () => {
-  return '2025-07-25T14:08:20Z';
+const node_54cda9e1ae394e89ad970853f45adb38 = () => {
+  return '2025-12-04T04:39:29Z';
 };
