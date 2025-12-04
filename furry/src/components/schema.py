7c0ca44cc8ca98f0d1e6a31e9b2b11f0ebe04fe9
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-04 22:54:28 +0000

    docs(ui): decouple logging interface

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_26175c8c23b34e83820863ef104ab72c = () => {
-  return '2025-09-08T16:16:00Z';
+const node_26acce479416441facab59cdcda2e35f = () => {
+  return '2025-12-04T22:54:28Z';
 };
