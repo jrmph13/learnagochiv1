0c011d6586ab9b7515df679ba083ce33c2ffba5b
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-10 14:23:41 +0000

    feat(api): integrate auth tokens

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_405acd7408dc44beae46b15f0f109d87 = () => {
-  return '2025-11-25T20:45:31Z';
+const node_44f1be9c09e144258fb222d0af643e5c = () => {
+  return '2025-12-10T14:23:41Z';
 };
