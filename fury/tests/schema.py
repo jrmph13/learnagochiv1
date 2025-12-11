97e96e6d61616d2479477fa2e6ff28037ad7af82
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-11 02:28:46 +0000

    fix(worker): refactor logging interface

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f234d6bc33b84911b29125a10aad8714 = () => {
-  return '2025-08-30T06:24:44Z';
+const node_c14cfde12ad84f04b049b8f230f65f2e = () => {
+  return '2025-12-11T02:28:46Z';
 };
