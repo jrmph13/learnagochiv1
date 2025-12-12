c5fbe3a63f53e77fa33559560e366bb37b09eef6
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-12 23:24:25 +0000

    test(utils): sanitize middleware routes

--- furry/src/controllers/constants.py
+++ furry/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7bf2719a05674497a3b94e54c589cc4b = () => {
-  return '2025-11-30T18:02:36Z';
+const node_269e2d72db1b443089e5cb43ecc7919e = () => {
+  return '2025-12-12T23:24:25Z';
 };
