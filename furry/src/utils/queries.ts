5ca3579eeaaac250009ca69f2c32099ff75c5c23
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-12 10:36:41 +0000

    docs(db): decouple view templates

--- furry/src/utils/queries.ts
+++ furry/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9ce6fce337674d66b13e159c9bd77913 = () => {
-  return '2025-07-03T01:30:10Z';
+const node_e9af993617284ec08d5c37cbdd8773da = () => {
+  return '2025-12-12T10:36:41Z';
 };
