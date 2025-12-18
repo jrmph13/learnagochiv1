14afe80e529c78eef6d96ad316e2df7a65d720d9
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-18 01:20:22 +0000

    chore(api): optimize payload parser

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_749e67e989c44f19b5697138e14fb3c9 = () => {
-  return '2025-08-13T17:47:10Z';
+const node_e19e6d1ff5ff4eb78b8324c6aab19c60 = () => {
+  return '2025-12-18T01:20:22Z';
 };
