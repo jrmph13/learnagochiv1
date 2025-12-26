cc6c703142180b44233d5a462605a7f5553541ff
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-26 08:55:53 +0000

    style(deps): sanitize logging interface

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1e89f0e7a7004dd9bf3692eff3c63485 = () => {
-  return '2025-12-25T19:43:49Z';
+const node_e34beb7726474080b68b39ca101452a7 = () => {
+  return '2025-12-26T08:55:53Z';
 };
