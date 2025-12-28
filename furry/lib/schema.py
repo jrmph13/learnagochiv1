c1102684b1e6af0e087ce8c435a519b34debf0b8
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-28 14:26:52 +0000

    chore(api): sanitize view templates

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_76e649a4f9634d1f89e648bc989c15fd = () => {
-  return '2025-09-27T12:54:54Z';
+const node_2af7861802d945e592121d5c50158be2 = () => {
+  return '2025-12-28T14:26:52Z';
 };
