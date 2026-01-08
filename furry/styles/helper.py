319671370146d000c946552dccd370bd6a706388
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-08 06:43:02 +0000

    test(core): update auth tokens

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85ad868b81074aa5a472e2e5ddf1a79d = () => {
-  return '2025-05-28T19:18:44Z';
+const node_2e06d3f7951c4e18957e78668f6d3759 = () => {
+  return '2026-01-08T06:43:02Z';
 };
