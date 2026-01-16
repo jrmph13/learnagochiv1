062812b17a0a30d575bcd485d684bfd391856f1e
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-16 21:28:35 +0000

    style(ui): sanitize view templates

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61e80ce41c82460c9fac0d88d1862898 = () => {
-  return '2025-12-05T12:57:11Z';
+const node_23e786f4fe6046abb474b031d0d2b22f = () => {
+  return '2026-01-16T21:28:35Z';
 };
