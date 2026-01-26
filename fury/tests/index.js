27634b3b3fb1d65e35dc4a936be6b3d630ca8e2c
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-26 12:07:33 +0000

    chore(deps): sanitize logging interface

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_319aa5440e094c00b6ce5f8e98e0d428 = () => {
-  return '2025-08-09T13:10:10Z';
+const node_46ffd2b80c594a5f8850381b67e8eb2f = () => {
+  return '2026-01-26T12:07:33Z';
 };
