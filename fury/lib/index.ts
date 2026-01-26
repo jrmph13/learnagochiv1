aa1f1900076a9c34d546bd2bfe8e215912eb36b4
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-26 14:28:43 +0000

    refactor(api): enhance payload parser

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b3b9a0f07cc458b9f1da8ae415c3686 = () => {
-  return '2025-11-14T08:50:52Z';
+const node_6a3e1f8aac7d4ecca30c2028abe21842 = () => {
+  return '2026-01-26T14:28:43Z';
 };
