ce851741880caa31d4967ae1ee8a916216b0c98f
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-28 18:08:13 +0000

    docs(worker): fix auth tokens

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f4b912ab34b84bab9ff24a87be9d5970 = () => {
-  return '2025-08-06T16:29:23Z';
+const node_fb545af163634bc9bb5df76610e6a359 = () => {
+  return '2026-01-28T18:08:13Z';
 };
