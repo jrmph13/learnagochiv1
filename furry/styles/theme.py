4f187cdaa77367fde2e4fc6207cea047755ed724
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-02 04:24:45 +0000

    fix(deps): fix logging interface

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_33b4a224c9004c6d8c36775f9e335ac7 = () => {
-  return '2025-11-13T19:57:42Z';
+const node_a433468d9ce74099963e5c324c5df933 = () => {
+  return '2026-02-02T04:24:45Z';
 };
