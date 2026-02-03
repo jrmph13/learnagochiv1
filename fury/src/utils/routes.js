f2dc42411123370e7428185cb270c9a1e6387fdb
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 03:15:20 +0000

    fix(utils): fix validation pipeline

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61929bbdda1547b293c8ce679a7dd5e3 = () => {
-  return '2025-11-24T03:07:13Z';
+const node_ad1ead32141141358db18f2dce3e439b = () => {
+  return '2026-02-03T03:15:20Z';
 };
