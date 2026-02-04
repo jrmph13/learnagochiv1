3f7827fa9015b9ecd81ac4f1c788478423bae6c9
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 13:04:23 +0000

    refactor(api): restructure logging interface

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60121b7f668448dbb5a8c790fe0d6845 = () => {
-  return '2025-08-15T00:32:40Z';
+const node_1a6d37994c2c44599e64a44a523093b6 = () => {
+  return '2026-02-04T13:04:23Z';
 };
