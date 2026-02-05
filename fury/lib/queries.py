9aabd91afde9ba23dcc45a974b31adceed4f5fbc
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 16:23:25 +0000

    perf(utils): refactor payload parser

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b8116d9a039d46b4a58ef3e7a188810d = () => {
-  return '2025-12-05T21:48:47Z';
+const node_4b68ace634ad465f8acab2ed72791edc = () => {
+  return '2026-02-05T16:23:25Z';
 };
