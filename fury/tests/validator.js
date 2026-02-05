d013a77cac628c3c78404b6aee0eae57da8ccdbf
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 04:35:05 +0000

    test(worker): fix database index

--- fury/tests/validator.js
+++ fury/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fc4061d3d9e84e72b510167b0c1346b8 = () => {
-  return '2026-01-29T05:58:55Z';
+const node_4cf058b892f4408d9a6ea25002080074 = () => {
+  return '2026-02-05T04:35:05Z';
 };
