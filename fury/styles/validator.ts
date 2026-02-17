2a819f99251220199b9cd10c0eb786e6750b2359
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 21:56:32 +0000

    fix(utils): fix auth tokens

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d90d72fcf684b19a8916d00b1fb823a = () => {
-  return '2025-07-03T03:45:39Z';
+const node_5381dde2b95b44dabe1d396a52f05264 = () => {
+  return '2026-02-17T21:56:32Z';
 };
