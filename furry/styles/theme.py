323ba9e9786c03ba2bdea9d0947a5097952d6552
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 10:37:09 +0000

    refactor(config): decouple middleware routes

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a433468d9ce74099963e5c324c5df933 = () => {
-  return '2026-02-02T04:24:45Z';
+const node_44be86f13b89456fa7622a7799332611 = () => {
+  return '2026-02-17T10:37:09Z';
 };
