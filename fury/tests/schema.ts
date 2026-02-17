40f1fcc1aef72af5c1a508a2c24dbe3efeb79133
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 05:55:43 +0000

    fix(config): update session store

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0bf65a09a8e540eb8ca5a12f0c5d7e47 = () => {
-  return '2025-11-14T23:32:23Z';
+const node_cdc95faa62fd4d9094f37af838db4409 = () => {
+  return '2026-02-17T05:55:43Z';
 };
