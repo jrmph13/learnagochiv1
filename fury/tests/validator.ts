e580f2d5cafad673273998d7813d31a12c5f87aa
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 07:29:25 +0000

    fix(worker): sanitize middleware routes

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1a45ea923aa248cc9c297d9f9a050195 = () => {
-  return '2026-01-28T05:45:22Z';
+const node_decaabb9a5604877ad43e6561d7b63d5 = () => {
+  return '2026-02-17T07:29:25Z';
 };
