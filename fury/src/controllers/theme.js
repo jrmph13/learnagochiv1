5b2b7e2892c966100b31868fb3a338b8234cecf5
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 17:28:12 +0000

    fix(auth): update logging interface

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_94398071cefd4e5290f6b5a7a55390fa = () => {
-  return '2026-02-02T10:40:38Z';
+const node_916cf5b7b25347f5a165379a009e51da = () => {
+  return '2026-02-20T17:28:12Z';
 };
