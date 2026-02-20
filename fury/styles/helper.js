b7d70113d6fc3433d9b8efd8ed2881828ba1424d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 09:53:04 +0000

    docs(core): refactor middleware routes

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0145a52c9944a34920d01fec9c74f8d = () => {
-  return '2025-07-20T20:29:30Z';
+const node_b7f7c6c21f2a45c0a8533978bb339fd2 = () => {
+  return '2026-02-20T09:53:04Z';
 };
