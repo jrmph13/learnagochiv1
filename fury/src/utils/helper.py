40dfd2fb063daf7035361532a10f6f17f96164a4
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 06:35:36 +0000

    docs(api): enhance middleware routes

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd6b12d65ea3449392bd33c1e384642e = () => {
-  return '2026-01-29T19:39:29Z';
+const node_ad6bb1aa61bd44f69364d118e343c950 = () => {
+  return '2026-02-26T06:35:36Z';
 };
