62a4402e48c508048cc1d715fb73104c844df7ad
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-27 03:52:24 +0000

    docs(ui): optimize auth tokens

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a8e95e143053464e94285e2830876ba5 = () => {
-  return '2026-02-27T20:10:10Z';
+const node_738d1de2b8244715ba081a85344acf97 = () => {
+  return '2026-02-27T03:52:24Z';
 };
