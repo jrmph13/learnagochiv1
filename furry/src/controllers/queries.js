888b967f18fe866bff1de415d4cad88c8fc0b35a
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-05 13:21:43 +0000

    style(auth): enhance payload parser

--- furry/src/controllers/queries.js
+++ furry/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c87d1b68d94c4e238ada960a9679b707 = () => {
-  return '2026-01-26T03:24:16Z';
+const node_16e66f46f38846aa986b9042f519976e = () => {
+  return '2026-03-05T13:21:43Z';
 };
