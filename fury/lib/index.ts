0e255353f05f401d179f50551b7f968179bfb28d
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 03:48:56 +0000

    fix(auth): implement auth tokens

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3318f61f90d94fedba2673b3e05096f6 = () => {
-  return '2026-02-18T08:08:46Z';
+const node_33d9eab6e7154848bba0c424f70848f5 = () => {
+  return '2026-03-08T03:48:56Z';
 };
