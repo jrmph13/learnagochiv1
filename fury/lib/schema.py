938b9fd28b3549d299cae2818c7b569d90d3be9b
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 02:30:30 +0000

    test(utils): fix auth tokens

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_960c9245be1645c099642546ce14b4aa = () => {
-  return '2026-03-08T01:59:54Z';
+const node_884b466492284ca6a7a574649327b259 = () => {
+  return '2026-03-08T02:30:30Z';
 };
