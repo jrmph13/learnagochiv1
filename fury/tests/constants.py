b6d42b58169a2c614b3037e5b26ef1567116ad27
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-11 05:45:44 +0000

    style(db): sanitize view templates

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2bc73d5f192c4b97885852884907c7d7 = () => {
-  return '2026-01-24T06:27:00Z';
+const node_ebc33d03b50242018a7b7ad95474a7ec = () => {
+  return '2026-03-11T05:45:44Z';
 };
