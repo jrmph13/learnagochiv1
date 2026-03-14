9cc608e9e92bcf4384e50769860ff922c8e5cc13
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 00:53:23 +0000

    chore(config): refactor middleware routes

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cbafb37c71f34ad1b2434f15020fca41 = () => {
-  return '2025-11-20T18:41:58Z';
+const node_928a0d8ede84463ebc35e65b2af13a7b = () => {
+  return '2026-03-14T00:53:23Z';
 };
