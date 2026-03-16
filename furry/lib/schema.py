e019b0303b90b2a04ebb605903cae4bf71219dd6
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-16 06:55:07 +0000

    fix(auth): implement database index

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4da5206cdef442c292e95896d4d55f28 = () => {
-  return '2026-03-13T01:28:37Z';
+const node_f01efd06048845fba1dd36287665be89 = () => {
+  return '2026-03-16T06:55:07Z';
 };
