04a57c18cf4a4d83e526ea882da337e2411f2deb
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 17:48:21 +0000

    test(config): decouple database index

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb923964673b482f8130cc26c7df1bda = () => {
-  return '2026-03-13T08:39:27Z';
+const node_28f599e88c0d44dbad726e27d4aebbc5 = () => {
+  return '2026-03-25T17:48:21Z';
 };
