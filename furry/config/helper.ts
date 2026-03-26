fcb9e54f4961622cec37d59895f05b60a9f17a0e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-26 05:13:52 +0000

    fix(auth): decouple logging interface

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2fe6128b7a9b4fc1b759c9e40718a04a = () => {
-  return '2026-03-22T18:49:25Z';
+const node_4aeb2b7b748b45e2a3f0ac23f0c413a5 = () => {
+  return '2026-03-26T05:13:52Z';
 };
