8185eab21191f96619c430ba1297cd0f2d530b39
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-01 04:24:22 +0000

    feat(api): restructure auth tokens

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1c832bceba8348e385997d0a6cc12e31 = () => {
-  return '2026-02-18T06:33:13Z';
+const node_8080604f9e544dd8b75b079fad2babb6 = () => {
+  return '2026-04-01T04:24:22Z';
 };
