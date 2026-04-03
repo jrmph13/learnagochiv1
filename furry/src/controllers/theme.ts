09afee20a89dcdc9ea6d8ea18813c55fd265d28e
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-03 06:41:47 +0000

    refactor(ui): restructure view templates

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f394703eef04af48cbf8291904c8b87 = () => {
-  return '2026-02-09T01:10:01Z';
+const node_463646933a7343e8ba1561a4cb64531c = () => {
+  return '2026-04-03T06:41:47Z';
 };
