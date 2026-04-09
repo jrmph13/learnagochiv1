0c78e0c1d4033f02c4792d54628b8241967e9ee4
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-09 17:10:05 +0000

    chore(core): fix view templates

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17e6792d66d748eb932811a1ac1f7652 = () => {
-  return '2026-04-02T18:06:46Z';
+const node_7c8d1c4386934f1a975c4bf87645549c = () => {
+  return '2026-04-09T17:10:05Z';
 };
