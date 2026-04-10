5e47eea7dd43e676b79cc93d48c93512dbfea868
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-10 19:49:39 +0000

    docs(core): restructure view templates

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c8d1c4386934f1a975c4bf87645549c = () => {
-  return '2026-04-09T17:10:05Z';
+const node_814c7b2ef10340e5883d5a7077e59b34 = () => {
+  return '2026-04-10T19:49:39Z';
 };
