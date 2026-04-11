d23c49969d03ebb994a0178bc8ed4abc9d1305e7
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-11 09:28:50 +0000

    style(auth): integrate auth tokens

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_76d7c4c4752545f6a08d62089c32fa8a = () => {
-  return '2026-03-20T06:32:58Z';
+const node_a22d2313d76c4b4fbfe7f70656aa219b = () => {
+  return '2026-04-11T09:28:50Z';
 };
