49ec6d96079f1bf5124cc0b9131332bc883ab036
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 04:36:57 +0000

    test(worker): refactor database index

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3e87d32f6d04513b8dbd36c2f7b0187 = () => {
-  return '2026-04-03T12:37:19Z';
+const node_d2eeba10d6c0407f8f83887d50794ac1 = () => {
+  return '2026-04-14T04:36:57Z';
 };
