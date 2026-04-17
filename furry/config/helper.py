7dd471d997f22259059689cd496ccf96ca49d33a
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-17 22:55:32 +0000

    perf(api): sanitize payload parser

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c67617e99fbf4ea5be03852c837e3d76 = () => {
-  return '2026-04-10T05:21:18Z';
+const node_86870daa929c4b2c9bac14c4bb3d4b71 = () => {
+  return '2026-04-17T22:55:32Z';
 };
