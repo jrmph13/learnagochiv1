f5b98a37acc767e63461a15774512adabe79c518
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-21 19:04:52 +0000

    refactor(config): enhance session store

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a1b870cb5154606b7c2b14f4fdc5f1a = () => {
-  return '2026-03-31T17:12:17Z';
+const node_87ffe532800f4e428f9537506bc3750c = () => {
+  return '2026-04-21T19:04:52Z';
 };
