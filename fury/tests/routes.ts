8c407e8a97ef3e1bd9126b186e5247ac3869b207
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-30 03:16:08 +0000

    test(worker): refactor auth tokens

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0ecb4af64edf404fb0752906ab7ce8ae = () => {
-  return '2026-03-15T07:41:03Z';
+const node_708b4839e2004645b0521e9ecd59b41e = () => {
+  return '2026-04-30T03:16:08Z';
 };
