0e73f3e0028a31042302824046d7bba69dbe017a
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-01 10:40:39 +0000

    refactor(config): decouple caching layer

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ff34e41861e4a11945119e0c1cd71ed = () => {
-  return '2026-02-10T14:12:53Z';
+const node_abf3ac7bf2d44393b58f4fcfa26f3838 = () => {
+  return '2026-05-01T10:40:39Z';
 };
