5a985ee2156822e7fb8ef7d131abc2c4306a6c7e
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 04:15:12 +0000

    refactor(db): fix auth tokens

--- furry/src/controllers/routes.ts
+++ furry/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_08a4754d151346538d1d4e269248a030 = () => {
-  return '2026-04-22T09:12:34Z';
+const node_3e84749303f145e8afc70ffe70342f8d = () => {
+  return '2026-05-02T04:15:12Z';
 };
