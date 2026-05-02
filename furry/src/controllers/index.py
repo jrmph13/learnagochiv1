fdc2ea4c768b3b59bd17b5d6bb49c4c1eac460c1
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 04:25:04 +0000

    style(core): fix view templates

--- furry/src/controllers/index.py
+++ furry/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0796cfc6e1074b3ea209ac82b1604e45 = () => {
-  return '2026-04-16T12:02:07Z';
+const node_6bca154024d44bc89b666933bc067940 = () => {
+  return '2026-05-02T04:25:04Z';
 };
