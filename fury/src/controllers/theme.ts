460e4397dc55db55bd5572e2b262e48a8a66f2fa
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-05 22:52:47 +0000

    perf(auth): decouple payload parser

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f232586802874f25af8904a7524aa2f9 = () => {
-  return '2025-12-08T15:20:42Z';
+const node_bc01f984253d4f41b37ebe6c9d1fdc2c = () => {
+  return '2026-05-05T22:52:47Z';
 };
