75297eb26cfca09daded30b0fcf460c13ee2f5a9
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 11:54:49 +0000

    fix(auth): fix middleware routes

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d547f0d1f5a4bf685bbea36ec0b6758 = () => {
-  return '2026-04-22T00:47:38Z';
+const node_c1bef643124541dfb7670315095606db = () => {
+  return '2026-05-06T11:54:49Z';
 };
