1d952aa6d672b7f32e57bdf3cd0e740f5590dae6
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 12:07:41 +0000

    refactor(auth): update view templates

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_94884d48f0ea46348df22cc8d1365241 = () => {
-  return '2026-01-28T04:49:35Z';
+const node_2d7f1afeb7a34caba9b01c5cd6c98370 = () => {
+  return '2026-05-06T12:07:41Z';
 };
