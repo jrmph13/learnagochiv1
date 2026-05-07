66b87bb58511107a37aa88cc726aa2deacd5ae85
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-07 16:54:44 +0000

    feat(db): restructure database index

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eec5029676324ee983d6c2b73d3eecf4 = () => {
-  return '2026-02-20T06:27:13Z';
+const node_1a8ea3572e9e4704a59c70c0ab50c796 = () => {
+  return '2026-05-07T16:54:44Z';
 };
