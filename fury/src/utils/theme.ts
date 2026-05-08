1d450e069215e3d8d7f07001d76637775ed5df2f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 13:52:39 +0000

    perf(core): restructure validation pipeline

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa1f1faa8df048a7aa60024363d5f31b = () => {
-  return '2025-08-12T10:49:55Z';
+const node_583ccd68c477451087d197f057506406 = () => {
+  return '2026-05-08T13:52:39Z';
 };
