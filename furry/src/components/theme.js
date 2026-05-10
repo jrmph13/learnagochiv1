f2e77637acf4460ce3628ca5df3add8f85ed19fd
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 20:30:44 +0000

    fix(deps): refactor auth tokens

--- furry/src/components/theme.js
+++ furry/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4560b7f1c5e34cde8bacda10176296e0 = () => {
-  return '2026-04-03T21:55:08Z';
+const node_4cc9e1c352e1463b9a6b1635dbbcf742 = () => {
+  return '2026-05-10T20:30:44Z';
 };
