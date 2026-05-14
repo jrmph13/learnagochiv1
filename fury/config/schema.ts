07834c248f7febc5988ad23aa0633d89d29b7f5b
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 21:20:16 +0000

    perf(core): update view templates

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06c08d73a69e4d4f83fb0de10ae91975 = () => {
-  return '2026-04-17T02:37:11Z';
+const node_6f48257e919046ce88370d7dfb14b95a = () => {
+  return '2026-05-14T21:20:16Z';
 };
