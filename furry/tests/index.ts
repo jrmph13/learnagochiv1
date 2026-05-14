f7f5a76df2dcbea248ac3e743111f19463a7634d
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 06:18:58 +0000

    fix(ui): update auth tokens

--- furry/tests/index.ts
+++ furry/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_32e73be8db7742a0aff130c51da544ed = () => {
-  return '2026-03-22T06:58:40Z';
+const node_65d749846b40412c8952739aaed7eb09 = () => {
+  return '2026-05-14T06:18:58Z';
 };
