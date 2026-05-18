5b0a70e332edfb5b7396c4fb34ed4e75efc06ea6
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 09:04:26 +0000

    refactor(api): sanitize view templates

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aca015534498459496a8ae2e8d87ef59 = () => {
-  return '2026-04-11T23:31:44Z';
+const node_17aa70a7fecd4606a75fc7a6fc0398c0 = () => {
+  return '2026-05-18T09:04:26Z';
 };
