bbc8281c4fe4abe4270081324f52c0ca603b9af1
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 14:03:57 +0000

    refactor(deps): update logging interface

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_72ea1548d86c4aed884a3896fcddbf7a = () => {
-  return '2026-03-27T17:01:14Z';
+const node_fc2f32e185b44741b5cb9fa84f9e81d9 = () => {
+  return '2026-05-19T14:03:57Z';
 };
