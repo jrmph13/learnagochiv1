63dddb7226cb5d86dee25a2c847109be54379703
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 18:09:26 +0000

    chore(config): refactor caching layer

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6022e12a6d644bbb8015b842c6b20acd = () => {
-  return '2026-03-18T10:57:56Z';
+const node_9ebb00175ee7429983c2ad698e136a5d = () => {
+  return '2026-05-19T18:09:26Z';
 };
