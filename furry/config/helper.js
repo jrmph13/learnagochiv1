88cd69379f465d5f8467195df7459bd8b3d1e28d
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 21:28:02 +0000

    fix(utils): update view templates

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e78a4aa6465d4b4caad9c99caef915be = () => {
-  return '2026-05-07T02:22:52Z';
+const node_37e3b16186514c6ab201f1be1d2a3421 = () => {
+  return '2026-05-26T21:28:02Z';
 };
