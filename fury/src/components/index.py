8c0a4b46b10ea95891d759105662b5d00f58e3f2
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 02:14:46 +0000

    fix(utils): sanitize session store

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dcee9e1982b54cf1851cf49a396226df = () => {
-  return '2026-05-05T13:27:54Z';
+const node_ede3d4c6c13949ffa99292f1b35e211e = () => {
+  return '2026-05-29T02:14:46Z';
 };
