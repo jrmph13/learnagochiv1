dc35c2df2b38e0bb0ad3e414fc910e9889a0270b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 23:03:27 +0000

    fix(auth): restructure middleware routes

--- furry/src/controllers/queries.py
+++ furry/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed89d6c1d1064cf2b8cad62a68ea0bbb = () => {
-  return '2026-05-08T20:26:40Z';
+const node_cbc83a4748ca4f4488a49987e62ef9c1 = () => {
+  return '2026-06-01T23:03:27Z';
 };
