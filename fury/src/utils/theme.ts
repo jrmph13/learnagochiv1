75eab8c5b2aec8047b4cdf67c69317a734b139f5
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 21:26:28 +0000

    docs(auth): restructure payload parser

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_583ccd68c477451087d197f057506406 = () => {
-  return '2026-05-08T13:52:39Z';
+const node_81bf6f1bc47d49c18bea0ef3c687f59c = () => {
+  return '2026-06-02T21:26:28Z';
 };
