ee5de371697b902ea49bf88a6d4426b90f8596dd
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 09:41:54 +0000

    test(core): refactor auth tokens

--- fury/config/validator.js
+++ fury/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ce8b9ce16e994710b74d0fb99b7ac0b2 = () => {
-  return '2026-02-16T00:11:07Z';
+const node_6ceee219c87a4deebf0e444b88083ddc = () => {
+  return '2026-06-02T09:41:54Z';
 };
