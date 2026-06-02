640a92a779bd3942525b4df5aeb9493dcf661923
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 00:45:43 +0000

    chore(auth): enhance view templates

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87ffe532800f4e428f9537506bc3750c = () => {
-  return '2026-04-21T19:04:52Z';
+const node_24325772a48b4f32ae5afada4fffb72a = () => {
+  return '2026-06-02T00:45:43Z';
 };
