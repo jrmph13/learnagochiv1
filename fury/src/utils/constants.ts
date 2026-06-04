2ed2ca8a9b6b89d9ca49fc479a9a6779ac60c6b9
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 04:32:08 +0000

    refactor(db): fix database index

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_715cfab8ce30490494d460b69b59998b = () => {
-  return '2026-05-10T20:38:10Z';
+const node_e5b1090b038b43399bd1320e4d72c76c = () => {
+  return '2026-06-04T04:32:08Z';
 };
