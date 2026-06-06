49699cb14a6a98924b964cb414f63a7facd78016
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 11:53:53 +0000

    perf(config): decouple payload parser

--- furry/lib/validator.py
+++ furry/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c0706f831eb44528a07d0a2863ed7dc9 = () => {
-  return '2026-05-15T02:54:28Z';
+const node_870c096789e749b29593ddcf07babec8 = () => {
+  return '2026-06-06T11:53:53Z';
 };
