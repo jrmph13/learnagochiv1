009d3b5a9ba57709d40e3441b0f4b1ffa06a7dcb
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-08 18:32:45 +0000

    perf(auth): fix view templates

--- furry/src/components/index.ts
+++ furry/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1f08dcc99e448a9947ceb7b6ab85957 = () => {
-  return '2026-04-03T05:00:54Z';
+const node_5951b45b00e94093817ec00665ef6232 = () => {
+  return '2026-06-08T18:32:45Z';
 };
