a39c5eccf041a9ad6af02be8165e5d2b2951d71d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 05:55:28 +0000

    fix(deps): sanitize session store

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_785ea004913f4042a044f5a72edc14b1 = () => {
-  return '2026-06-03T01:44:44Z';
+const node_4128ac610e974ea8bd435849fdd94f8e = () => {
+  return '2026-06-10T05:55:28Z';
 };
