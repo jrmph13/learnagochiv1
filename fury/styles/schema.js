3e8d9e02e7783a3f6eb15dbe4469ec28b46b918c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 08:39:54 +0000

    refactor(worker): sanitize auth tokens

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_669e7353d5fa4984804e8d9d9608a30d = () => {
-  return '2026-06-14T21:36:50Z';
+const node_32294fecd08743e28d374f49ae54fdde = () => {
+  return '2026-06-14T08:39:54Z';
 };
