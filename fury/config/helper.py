b06295b13718ed27e8242a63b720f71e3d841490
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 09:14:35 +0000

    docs(config): enhance session store

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e81e9d9ba8de4cb29d7144355f33515c = () => {
-  return '2025-07-29T06:44:42Z';
+const node_b3973a03be9a4cdc83676a358c6d54bc = () => {
+  return '2026-06-16T09:14:35Z';
 };
