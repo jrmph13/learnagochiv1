bde622b14aa295b0fc975d15b04954891778356c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 12:50:25 +0000

    perf(deps): restructure payload parser

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de87aab97f4c447faf9fed4d151a8c91 = () => {
-  return '2026-03-24T11:20:46Z';
+const node_5213a23493c6420084af6f77788d8ee0 = () => {
+  return '2026-06-19T12:50:25Z';
 };
