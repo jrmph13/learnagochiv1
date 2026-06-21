439c37ba1b9d5c849978beec09d4b903d123885f
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 03:29:47 +0000

    feat(auth): optimize payload parser

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3676f5e4cf2486cad51811d56902e01 = () => {
-  return '2026-03-10T14:23:08Z';
+const node_d4a213ded3d04d6c8ce1c59db93a71d1 = () => {
+  return '2026-06-21T03:29:47Z';
 };
