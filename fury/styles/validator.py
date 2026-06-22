21a660efdf5793cbd989829be556c1f26336251d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 15:05:22 +0000

    feat(auth): decouple middleware routes

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03d50f1e84814d4bba51dd603dd3025b = () => {
-  return '2026-05-06T21:32:49Z';
+const node_1d208ebe0e1b4a229f823f6f98be102c = () => {
+  return '2026-06-22T15:05:22Z';
 };
