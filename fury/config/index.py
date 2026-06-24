5431edfaebb8ed373f741d8df0f58e2ae02ec3bf
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-24 09:56:57 +0000

    test(auth): refactor payload parser

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa6ac48b27c44ee9a88ed3ee2227155f = () => {
-  return '2026-06-09T13:54:34Z';
+const node_f5589957f66d4856abc53db8c995bcbb = () => {
+  return '2026-06-24T09:56:57Z';
 };
