b1e352f8e88e2d37f0ee4d3d093ba513be7ec4a8
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 23:20:46 +0000

    feat(auth): refactor payload parser

--- furry/src/components/index.ts
+++ furry/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fa88ad0a30af4416be9527249108b899 = () => {
-  return '2026-06-20T17:10:26Z';
+const node_2da6841519364552acfc56d4bfa87131 = () => {
+  return '2026-06-25T23:20:46Z';
 };
