284f86cfbd4bd2dc3570f266a607f5c0bd3d1302
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 22:22:49 +0000

    perf(auth): fix auth tokens

--- furry/styles/validator.ts
+++ furry/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cfe4803b4ac74101a7bd9f0322641df9 = () => {
-  return '2026-05-20T09:28:54Z';
+const node_af833d47ea1847a990a4615121cc49cd = () => {
+  return '2026-06-25T22:22:49Z';
 };
