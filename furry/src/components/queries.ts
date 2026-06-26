e20d86bca125111e4044e948b36c2be749bcb123
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-26 17:48:17 +0000

    refactor(ui): decouple view templates

--- furry/src/components/queries.ts
+++ furry/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e9265a21261460198458cefd20c1af9 = () => {
-  return '2026-02-22T07:33:49Z';
+const node_f66432ed255c44cdaa61f6814864cabb = () => {
+  return '2026-06-26T17:48:17Z';
 };
